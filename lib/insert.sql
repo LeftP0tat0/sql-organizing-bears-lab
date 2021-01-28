d983e5989cc76e5aaf6a4bd824868a584ff5f10b
Vivian Stanley <vivianstanley@Vivians-MacBook-Air.local>
2021-01-27 19:43:03 -0800

    commit tree e0f44b10a57872ff6c3b898983cfba443d531146

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive ) VALUES (1,"Mr. Chocolate", 2, "M", "brown", "nice", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive ) VALUES (2, "Rowdy", 8, "F", "black", "goofy", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive ) VALUES (3, "Tabitha", 4, "F", "black", "lazy", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive ) VALUES (4, "Sereant Brown", 5, "M", "brown", "goofy", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive ) VALUES (5, "Melissa", 3, "F", "brown", "nice", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive ) VALUES (6, "Grinch", 5, "M", "black", "snappy", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive ) VALUES (7, "Wendy", 6, "F", "brown", "nice", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive ) VALUES (8, NULL, 3, "M", "brown", "aggressive", false);
